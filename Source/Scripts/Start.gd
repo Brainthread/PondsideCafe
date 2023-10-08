affa688480d8812a7934d4d87ff15d2754859d98
Julia Wang <julia.v.wang@gmail.com>
2023-10-08 19:41:08 +0200

    Main menu layout

---
+++ Source/Scripts/Start.gd
@@ -0,0 +1,14 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Stages/café.tscn")
